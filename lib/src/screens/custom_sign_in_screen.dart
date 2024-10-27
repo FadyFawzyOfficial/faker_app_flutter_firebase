eae76042c8dad0a8b11069fe01d5a4a88f7ebaf8
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-10-27 13:34:55 +0300

    2. Implement Firebase authentication email/password method with its design via firebase_ui_auth package

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,10 +7,17 @@ class CustomSignInScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    //* Note that declaring final authProviders = [EmailAuthProvider()] on each
+    //* widget that needs it can lead to code duplication and subtle bugs.
+    //* We will refactor this later on.
+    final authProviders = <AuthProvider>[
+      EmailAuthProvider(),
+      // PhoneAuthProvider()
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign in'),
-      ),
+      appBar: AppBar(title: const Text('Sign in')),
+      body: SignInScreen(providers: authProviders),
     );
   }
 }

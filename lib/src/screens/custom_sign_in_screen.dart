81e2b5843facd6dd082da121cb92b63d5af6642f
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-10-27 17:45:10 +0300

    4. Navigate to ProfileScreen when sign in or sign up is successful

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../routing/app_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
@@ -17,7 +20,19 @@ class CustomSignInScreen extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Sign in')),
-      body: SignInScreen(providers: authProviders),
+      body: SignInScreen(
+        providers: authProviders,
+        // To navigate to a different page when sign-in or register is successful,
+        // we can use AuthStateChangeAction callbacks
+        actions: [
+          AuthStateChangeAction<SignedIn>(
+            (context, state) => context.goNamed(AppRoute.profile.name),
+          ),
+          AuthStateChangeAction<UserCreated>(
+            (context, state) => context.goNamed(AppRoute.profile.name),
+          ),
+        ],
+      ),
     );
   }
 }

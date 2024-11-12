d80b11be5a8ff53f4ea4a1fc5dbfffb2c493da3b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-11-12 09:56:45 +0200

    2. Create a separate provider for authProviders rather than declaring in multiple files

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -2,18 +2,14 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'ui_auth_providers.dart';
+
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    //* Note that declaring final authProviders = [EmailAuthProvider()] on each
-    //* widget that needs it can lead to code duplication and subtle bugs.
-    //* We will refactor this later on.
-    final authProviders = <AuthProvider>[
-      EmailAuthProvider(),
-      // PhoneAuthProvider()
-    ];
+    final authProviders = ref.watch(authProvidersProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Sign in')),

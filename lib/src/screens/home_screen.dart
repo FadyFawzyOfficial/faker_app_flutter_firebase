64662817ad67052a8b0208fb272d6b6854e2c32b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-11-13 17:47:47 +0200

    1. (Exercise) Convert the JobsListView widget to use FirestoreQueryBuilder instead of FirestoreListView

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../data/firestore_repo.dart';
-import '../data/job.dart';
 import '../routing/app_router.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -44,41 +43,96 @@ class JobsListView extends ConsumerWidget {
   Widget build(context, ref) {
     final firestoreRepo = ref.watch(firestoreRepoProvider);
     final user = ref.watch(firebaseAuthProvider).currentUser;
-    return FirestoreListView<Job>(
+    return FirestoreQueryBuilder(
       query: firestoreRepo.jobsQuery(uid: user!.uid),
-      errorBuilder: (_, error, __) => Center(child: Text('$error')),
-      emptyBuilder: (_) => const Center(child: Text('No data')),
-      itemBuilder: (_, doc) {
-        final job = doc.data();
-        return Dismissible(
-          key: Key(doc.id),
-          background: const ColoredBox(color: Colors.red),
-          onDismissed: (_) =>
-              firestoreRepo.deleteJob(uid: user.uid, jobId: doc.id),
-          child: ListTile(
-            title: Text(job.title),
-            subtitle: Text(job.company),
-            trailing: job.createdDate != null
-                ? Text(
-                    '${job.createdDate}',
-                    style: Theme.of(context).textTheme.bodySmall,
-                  )
-                : null,
-            onTap: () {
-              final faker = Faker();
-              final title = faker.job.title();
-              final company = faker.company.name();
-              final user = ref.read(firebaseAuthProvider).currentUser;
-              firestoreRepo.updateJob(
-                jobId: doc.id,
-                uid: user!.uid,
-                title: title,
-                company: company,
-              );
-            },
-          ),
+      builder: (context, snapshot, _) {
+        if (snapshot.isFetching) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('${snapshot.error}'));
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.docs.length,
+          itemBuilder: (context, index) {
+            // if we reached the end of the currently obtained items, we try to
+            // obtain more items
+            if (snapshot.hasMore && index + 1 == snapshot.docs.length) {
+              // Tell FirestoreQueryBuilder to try to obtain more items.
+              // It is safe to call this function from within the build method.
+              snapshot.fetchMore();
+            }
+            final doc = snapshot.docs[index];
+            final job = doc.data();
+            return Dismissible(
+              key: Key(doc.id),
+              background: const ColoredBox(color: Colors.red),
+              onDismissed: (_) =>
+                  firestoreRepo.deleteJob(uid: user.uid, jobId: doc.id),
+              child: ListTile(
+                title: Text(job.title),
+                subtitle: Text(job.company),
+                trailing: job.createdDate != null
+                    ? Text(
+                        '${job.createdDate}',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      )
+                    : null,
+                onTap: () {
+                  final faker = Faker();
+                  final title = faker.job.title();
+                  final company = faker.company.name();
+                  final user = ref.read(firebaseAuthProvider).currentUser;
+                  firestoreRepo.updateJob(
+                    jobId: doc.id,
+                    uid: user!.uid,
+                    title: title,
+                    company: company,
+                  );
+                },
+              ),
+            );
+          },
         );
       },
     );
+
+    // FirestoreListView<Job>(
+    //   query: firestoreRepo.jobsQuery(uid: user!.uid),
+    //   errorBuilder: (_, error, __) => Center(child: Text('$error')),
+    //   emptyBuilder: (_) => const Center(child: Text('No data')),
+    //   itemBuilder: (_, doc) {
+    //     final job = doc.data();
+    //     return Dismissible(
+    //       key: Key(doc.id),
+    //       background: const ColoredBox(color: Colors.red),
+    //       onDismissed: (_) =>
+    //           firestoreRepo.deleteJob(uid: user.uid, jobId: doc.id),
+    //       child: ListTile(
+    //         title: Text(job.title),
+    //         subtitle: Text(job.company),
+    //         trailing: job.createdDate != null
+    //             ? Text(
+    //                 '${job.createdDate}',
+    //                 style: Theme.of(context).textTheme.bodySmall,
+    //               )
+    //             : null,
+    //         onTap: () {
+    //           final faker = Faker();
+    //           final title = faker.job.title();
+    //           final company = faker.company.name();
+    //           final user = ref.read(firebaseAuthProvider).currentUser;
+    //           firestoreRepo.updateJob(
+    //             jobId: doc.id,
+    //             uid: user!.uid,
+    //             title: title,
+    //             company: company,
+    //           );
+    //         },
+    //       ),
+    //     );
+    //   },
+    // );
   }
 }

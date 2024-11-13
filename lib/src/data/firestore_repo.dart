1a1e9cb16ebb897b8ace5237fb7b3dfbffe80064
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-11-13 14:34:03 +0200

    1. Add updateJob() method to FirestoreRepo to update a document

--- lib/src/data/firestore_repo.dart
+++ lib/src/data/firestore_repo.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'job.dart';
@@ -13,12 +12,20 @@ class FirestoreRepo {
     required String uid,
     required String title,
     required String company,
-  }) async {
-    final jobDoc = await _firestore
-        .collection('jobs')
-        .add(Job(uid: uid, title: title, company: company).toMap());
-    debugPrint(jobDoc.id);
-  }
+  }) async =>
+      await _firestore
+          .collection('jobs')
+          .add(Job(uid: uid, title: title, company: company).toMap());
+
+  Future<void> updateJob({
+    required String jobId,
+    required String uid,
+    required String title,
+    required String company,
+  }) async =>
+      await _firestore
+          .doc('jobs/$jobId')
+          .update(Job(uid: uid, title: title, company: company).toMap());
 
   Query<Job> get jobsQuery => _firestore.collection('jobs').withConverter(
         fromFirestore: (snapshot, options) => Job.fromMap(snapshot.data()!),
